50fdd00a25f6427bedf1483341ff0ae37d731f26
Siddhi Sahu <himanisahu739@gmail.com>
2025-02-12 13:37:24 +0530

    add ws server

--- Backend1/src/index.ts
+++ Backend1/src/index.ts
@@ -1,2 +1,14 @@
 //ws server
 
+import { WebSocketServer } from "ws";
+import { GameManager } from "./GameManager";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+const gameManager = new GameManager();
+
+wss.on("connection", function connection(ws) {
+    gameManager.addUser(ws);
+
+    ws.on("disconnect", () => gameManager.removeUser(ws));
+})
