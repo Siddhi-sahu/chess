815daae535773b73561051a5466cc089f3b8e51f
Siddhi Sahu <himanisahu739@gmail.com>
2025-02-15 12:22:26 +0530

    commit

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,8 +1,10 @@
 
 
+import { useNavigate } from "react-router-dom";
 import ChessBoard from "../assets/chessBoard.jpeg";
 
 export const Landing = () => {
+    const navigate = useNavigate();
     return (
         <div className="min-h-screen w-full flex justify-center items-center bg-[#2a1a0a]">
             <div className="grid grid-cols-1 md:grid-cols-2 w-full max-w-6xl shadow-lg rounded-lg overflow-hidden bg-[#3e260e]">
@@ -13,7 +15,7 @@ export const Landing = () => {
 
 
                 <div className="bg-[#5a3d1e] flex flex-col items-center justify-center text-center p-10 text-gold">
-                    <button className="mb-6 bg-gradient-to-r from-[#c29f59] to-[#e6c37b] text-[#3e260e] font-bold px-8 py-3 rounded-full shadow-lg transition-all hover:scale-105 hover:shadow-xl">
+                    <button className="mb-6 bg-gradient-to-r from-[#c29f59] to-[#e6c37b] text-[#3e260e] font-bold px-8 py-3 rounded-full shadow-lg transition-all hover:scale-105 hover:shadow-xl" onClick={() => navigate("/game")}>
                         Play Now
                     </button>
                     <h1 className="text-4xl md:text-5xl text-[#f3e2c7] font-bold leading-tight drop-shadow-lg">
